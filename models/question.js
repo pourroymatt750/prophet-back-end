75782866898f8344cda060400258218f287444e5
Matthew Pourroy <pourroy.matt@gmail.com>
2022-07-25 14:41:19 -0400

    add question model;

---
+++ models/question.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const answerSchema = new mongoose.Schema({
+  answer: {
+    type: String, 
+    required: true
+  }
+},{
+  timestamps: true,
+})
+
+const questionSchema = new mongoose.Schema({
+  question: {
+    type: String, 
+    required: true
+  },
+  answers: [answerSchema],
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+},{
+  timestamps: true,
+})
+
+const Question = mongoose.model('Question', questionSchema)
+
+export { Question }
