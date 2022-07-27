485705706f5eec6c9035c9e1abaf557ba5b98c56
Matthew Pourroy <pourroy.matt@gmail.com>
2022-07-27 09:45:59 -0400

    back end crud working

--- controllers/questions.js
+++ controllers/questions.js
@@ -15,3 +15,52 @@ export function create(req, res) {
     res.status(500).json({err: err.errmsg})
   })
 }
+
+export function index(req,res) {
+  Question.find({})
+  .populate('owner')
+  .then(questions => {
+    res.json(questions)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
+
+export function deleteOne(req, res) {
+  Question.findById(req.params.id)
+  .then(question => {
+    if (question.owner._id.equals(req.user.profile)) {
+      Question.findByIdAndDelete(question._id)
+      .then(deletedQuestion => {
+        res.json(deletedQuestion)
+      })
+    } else {
+      res.status(401).json({err: "Not authorized"})
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
+
+export function update(req, res) {
+  Question.findById(req.params.id)
+  .then(question => {
+    if (question.owner._id.equals(req.user.profile)) {
+      Question.findByIdAndUpdate(req.params.id, req.body, {new: true})
+      .populate('owner')
+      .then(updatedQuestion => {
+        res.json(updatedQuestion)
+      })
+    } else {
+      res.status(401).json({err: "Not authorized"})
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
--- routes/questions.js
+++ routes/questions.js
@@ -5,10 +5,12 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
+router.get('/', questionsCtrl.index)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, questionsCtrl.create)
+router.delete('/:id', checkAuth, questionsCtrl.deleteOne)
+router.put('/:id', questionsCtrl.update)
 
 export { router }
